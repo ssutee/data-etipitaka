ac51800fce68decf57667c91a8aebc1b072241c6
Sutee Sudprasert <sutee.s@gmail.com>
2026-05-21 10:55:06 +0700

    refactor: Python 3 / Django 5.2 compatible models

--- app/user_data/models.py
+++ app/user_data/models.py
@@ -1,30 +1,30 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
 
-from urlparse import urljoin
 
 def user_directory_path(instance, filename):
     return '%s/%s/%s' % (instance.user.username, instance.platform, filename)
 
+
 class UserData(models.Model):
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     platform = models.TextField()
     file = models.FileField(upload_to=user_directory_path)
     deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
+
 class SyncData(models.Model):
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     name = models.TextField()
     checksum = models.TextField(blank=True, null=True)
     platform = models.TextField()
     file = models.FileField(upload_to=user_directory_path)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
-class Sharing(models.Model):
-    owner = models.ForeignKey(User, null=True, related_name='sharing_owners')
-    follower = models.ForeignKey(User, null=True, related_name='sharing_followers')
 
+class Sharing(models.Model):
+    owner = models.ForeignKey(User, null=True, related_name='sharing_owners',
+                              on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, null=True, related_name='sharing_followers',
+                                 on_delete=models.CASCADE)
