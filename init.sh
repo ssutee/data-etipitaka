7a8cb97f0056ca4a30e414c7936ea5dabcf7d411
Sutee Sudprasert <sutee.s@gmail.com>
2026-05-21 11:36:32 +0700

    build: filtered seed fixture and docker-compose init script

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-docker-compose run --rm web python manage.py migrate
-docker-compose run --rm web python manage.py collectstatic
-docker-compose run --rm web python manage.py loaddata dump.json
-
+docker compose run --rm web python manage.py migrate --noinput
+docker compose run --rm web python manage.py collectstatic --noinput
+docker compose run --rm web python manage.py loaddata seed.json
